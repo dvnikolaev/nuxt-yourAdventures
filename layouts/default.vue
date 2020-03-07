d171e5c57bb23f276563981b16cff074f2271010
dvnikolaev <dvnikolaeev@gmail.com>
2020-03-07 13:44:37 +0400

    feat: add methods registration login logout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,24 +2,52 @@
   <v-app>
     <v-app-bar app dense>
       <v-spacer></v-spacer>
-      <v-dialog v-for="(item, i) in navIconInfo" :key="i" width="350">
-        <template v-slot:activator="{ on: dialog }">
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on: tooltip }">
-              <v-btn icon v-on="{...tooltip, ...dialog}">
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-btn>
-            </template>
-            <span>{{ item.text }}</span>
-          </v-tooltip>
+      <v-tooltip bottom v-if="userIsAuth">
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on" @click="logout">
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
         </template>
-        <v-card v-model="item.dialog" class="py-7 px-6 text-center d-flex flex-column">
-          <h2>{{ item.text }}</h2>
-          <v-text-field outlined hide-details label="Логин" class="mt-2"></v-text-field>
-          <v-text-field outlined hide-details label="Пароль" class="mt-2"></v-text-field>
-          <v-btn @click="item.method" large color="success" class="mt-3">{{ item.textButton }}</v-btn>
-        </v-card>
-      </v-dialog>
+        <span>Выход</span>
+      </v-tooltip>
+      <div v-else>
+        <v-dialog v-for="(item, i) in navIconInfo" :key="i" width="350">
+          <template v-slot:activator="{ on: dialog }">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on: tooltip }">
+                <v-btn icon v-on="{ ...tooltip, ...dialog }">
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-btn>
+              </template>
+              <span>{{ item.text }}</span>
+            </v-tooltip>
+          </template>
+          <v-card
+            v-model="item.dialog"
+            class="py-7 px-6 text-center d-flex flex-column"
+          >
+            <h2>{{ item.text }}</h2>
+            <v-text-field
+              outlined
+              hide-details
+              label="Логин"
+              v-model="username"
+              class="mt-2"
+            ></v-text-field>
+            <v-text-field
+              outlined
+              hide-details
+              label="Пароль"
+              type="password"
+              v-model="password"
+              class="mt-2"
+            ></v-text-field>
+            <v-btn @click="item.method" large color="success" class="mt-3">{{
+              item.textButton
+            }}</v-btn>
+          </v-card>
+        </v-dialog>
+      </div>
     </v-app-bar>
     <v-content>
       <nuxt />
@@ -34,6 +62,8 @@
 export default {
   data() {
     return {
+      username: "",
+      password: "",
       navIconInfo: [
         {
           text: "Регистрация",
@@ -50,12 +80,26 @@ export default {
       ]
     };
   },
+  computed: {
+    userIsAuth() {
+      return this.$store.state.user.username ? true : false;
+    }
+  },
   methods: {
     registration() {
-      console.log(`registration`)
+      this.$store.dispatch("registration", {
+        username: this.username,
+        password: this.password
+      });
     },
     login() {
-      console.log('login')
+      this.$store.dispatch("login", {
+        username: this.username,
+        password: this.password
+      });
+    },
+    logout() {
+      this.$store.dispatch("logout");
     }
   }
 };
