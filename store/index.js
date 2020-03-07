c8a0adab3f4f4f164c5e5e9ddd3bf951e2422a88
dvnikolaev <dvnikolaeev@gmail.com>
2020-03-07 13:46:56 +0400

    feat: create store

---
+++ store/index.js
@@ -0,0 +1,58 @@
+export const state = () => ({
+  user: {
+    username: "",
+    adventures: [],
+    favoriteAdventures: []
+  }
+});
+
+export const mutations = {
+  SET_USER(state, payload) {
+    state.user = payload.user;
+  },
+  SET_ERROR_LOGIN(state, error) {
+    console.log("SET_ERROR_LOGIN");
+  },
+  SET_ERROR_REGISTER(state, error) {
+    console.log("SET_ERROR_REGISTER");
+  },
+  CHANGE_DIALOG_SUCCESSFUL_REGISTRATION(state) {
+    console.log("CHANGE_DIALOG_SUCCESSFUL_REGISTRATION");
+  }
+};
+
+export const actions = {
+  // Регистрация пользователя
+  async registration({ commit }, data) {
+    try {
+      let error = await this.$axios.$post("/registration", {
+        username: data.username,
+        password: data.password
+      });
+      error
+        ? commit("SET_ERROR_REGISTER", error)
+        : commit("CHANGE_DIALOG_SUCCESSFUL_REGISTRATION");
+    } catch {
+      commit(
+        "SET_ERROR_REGISTER",
+        "Не удалось зарегистрироваться, попробуйте позже!"
+      );
+    }
+  },
+  async login({ commit }, data) {
+    try {
+      let response = await this.$axios.$post("/login", {
+        username: data.username,
+        password: data.password
+      });
+      commit("SET_USER", response);
+      commit("SET_ERROR_LOGIN", '');
+    } catch {
+      commit("SET_ERROR_LOGIN", 'Неверный пользователь или пароль');
+    }
+  },
+  async logout({commit}) {
+    let response = await this.$axios.$post("/logout");
+    commit("SET_USER", response);
+  }
+};
