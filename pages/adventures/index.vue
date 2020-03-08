da3b5fa6e860355ff02b91236edabac07f27c06b
dvnikolaev <dvnikolaeev@gmail.com>
2020-03-08 12:48:27 +0400

    feat: data from store

--- pages/adventures/index.vue
+++ pages/adventures/index.vue
@@ -1,18 +1,28 @@
 <template>
-  <v-container fluid>
-    <CardAdventure />
+  <v-container fluid class="">
+    <v-row>
+      <v-col v-for="(item, i) in adventures" :key="i" cols="12" lg="3" md="4" sm="6" xs="12">
+        <CardAdventure
+          :title="item.title"
+          :description="item.description"
+          :image="item.image"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import CardAdventure from '@/components/Card/CardAdventure'
+import CardAdventure from "@/components/Card/CardAdventure";
 
 export default {
+  computed: {
+    adventures() {
+      return this.$store.state.adventures;
+    }
+  },
   components: {
     CardAdventure
   }
-}
+};
 </script>
-
-<style>
-</style>
