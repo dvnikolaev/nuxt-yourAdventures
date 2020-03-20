13b720b77e98962ae068417362542df64d6fc7dc
dvnikolaev <dvnikolaeev@gmail.com>
2020-03-20 23:05:25 +0400

    style: pretty

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,20 +3,25 @@
     <v-app-bar app dense style="z-index: 10">
       <DialogAddAdventure />
       <v-spacer></v-spacer>
-      <v-tooltip bottom v-if="userIsAuth">
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on" @click="logout">
-            <v-icon>mdi-logout</v-icon>
-          </v-btn>
-        </template>
-        <span>Выход</span>
-      </v-tooltip>
+      <div v-if="userIsAuth">
+        <a href="/">{{getUser}}</a>
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-btn icon v-on="on" @click="logout">
+              <v-icon>mdi-logout</v-icon>
+            </v-btn>
+          </template>
+          <span>Выход</span>
+        </v-tooltip>
+      </div>
       <div v-else>
-        <DialogAuth v-for="(item,i) in navIconInfo" :key="i" 
-        :text="item.text"
-        :icon="item.icon"
-        :textButton="item.textButton"
-        :method="item.method"
+        <DialogAuth
+          v-for="(item, i) in navIconInfo"
+          :key="i"
+          :text="item.text"
+          :icon="item.icon"
+          :textButton="item.textButton"
+          :method="item.method"
         />
       </div>
     </v-app-bar>
@@ -43,23 +48,23 @@ export default {
           text: "Регистрация",
           textButton: "Зарегестрироваться",
           icon: "mdi-account-multiple-plus",
-          method: 'registration'
+          method: "registration"
         },
         {
           text: "Вход",
           textButton: "Войти",
           icon: "mdi-login",
-          method: 'login'
+          method: "login"
         }
       ]
     };
   },
-  created() {
-    this.$store.dispatch('getInfo');
-  },
   computed: {
     userIsAuth() {
       return this.$store.state.user.username ? true : false;
+    },
+    getUser() {
+      return this.$store.state.user.username;
     }
   },
   methods: {
