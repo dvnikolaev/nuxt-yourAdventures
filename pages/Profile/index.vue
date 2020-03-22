f8862c360f1b7643851f5137b86fe56b726613b3
dvnikolaev <dvnikolaeev@gmail.com>
2020-03-22 14:11:22 +0400

    style: pretty

--- pages/Profile/index.vue
+++ pages/Profile/index.vue
@@ -5,7 +5,7 @@
     <p v-if="!myAdventures.length">У вас пока нет добавленных записей</p>
     <v-row v-else>
       <v-col
-        v-for="(item,i) in myAdventures"
+        v-for="(item, i) in myAdventures"
         :key="i"
         cols="12"
         lg="3"
@@ -19,7 +19,7 @@
     <h2>Понравившиеся записи:</h2>
     <v-row>
       <v-col
-        v-for="(item,i) in myFavoriteAdventures"
+        v-for="(item, i) in myFavoriteAdventures"
         :key="i"
         cols="12"
         lg="3"
@@ -40,7 +40,7 @@ export default {
   async fetch({ store }) {
     await store.dispatch("getAdventures");
   },
-  middleware({ redirect, req, store}) {
+  middleware({ redirect, req, store }) {
     if (process.server) {
       if (req.user) {
         if (store.state.user.id !== req.user._id) {
